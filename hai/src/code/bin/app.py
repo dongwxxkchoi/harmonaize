578c6fdb1ccd3121b6eb71d6b7845c7f1c4bdbf9
dongwook <dwchoi0610@gmail.com>
2024-05-02 13:42:38 +0900

    🎨 changed router

--- hai/src/code/bin/app.py
+++ hai/src/code/bin/app.py
@@ -5,10 +5,9 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from web_api import basicpitch_router, getmusic_router
+from web_api import router, getmusic_router
 
 ########################################################
 # start server
 app = FastAPI()
-app.include_router(basicpitch_router.router)
-app.include_router(getmusic_router.router)
+app.include_router(router.router)
